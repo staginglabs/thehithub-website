5ea5e203869b2b245ec5124d06fe0a130cddf8a4
hariompatidar2001 <muteweb.hariom@gmail.com>
2026-01-01 19:17:11 +0530

    button update

--- src/fonts/fonts.ts
+++ src/fonts/fonts.ts
@@ -11,12 +11,7 @@ export const poppins = Poppins({
 export const bodyStyle = 'text-2xl leading-9 text-[#0F351D] antialiased';
 
 //buttons
-<<<<<<< HEAD
 export const primary_button = 'flex items-center justify-center rounded-full bg-[#18A34B] text-white py-3.5 pl-6.5 pr-2.5 h-[50px] text-base leading-[1em] font-medium hover:bg-green-700 hover:text-white gap-2.5';
-=======
-export const secondary_button = 'flex items-center justify-center  rounded-full bg-[#F8E108] py-2.5 pl-6.5 pr-2.5  text-base leading-[1em] font-bold hover:bg-green-700 hover:text-white lg:flex gap-2.5';
-export const primary_button = 'flex items-center justify-center rounded-full bg-[#18A34B] text-white py-2.5 pl-4.5 pr-3 text-base leading-[1em] font-medium hover:bg-green-700 hover:text-white lg:flex gap-2.5';
->>>>>>> 0e994e357e55346297edd5adc418165946b3a223
 
 export const secondary_button = 'flex items-center justify-center rounded-full bg-[#F8E108] py-3.5 pl-6.5 pr-2.5 h-[50px] text-base leading-[1em] font-bold hover:bg-green-700 hover:text-white gap-2.5';
 
